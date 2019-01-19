9ab8edaad4f3373406df28591cf5ff379c20e450
Luyen Duc Manh <luyenducmanh@gmail.com>
2019-01-20 00:12:45 +0700

    sualai

--- XepHang.py
+++ XepHang.py
@@ -2,7 +2,6 @@
 from markov.M_M_1 import M_M_1
 from markov.M_M_1_K import M_M_1_K
 from markov.M_M_S import M_M_S
-from markov.M_M_S_K import M_M_S_K
 import os
 import sys
 
@@ -87,35 +86,6 @@ def display_M_M_1_k():
     if i == '1':
        clear()
        display_M_M_1_k()
-    elif i == '2':
-        clear()
-        displayMenu()
-
-def display_M_M_s_k():
-    print("Mô hình hàng đợi giới hạn kích thước nhiều kênh phục vụ")
-    try:
-        s = float(input("So kenh phuc vu:"))
-        x = float(input("So khach hang den trung binh:"))
-        y = float(input("So khach hang duoc phuc trung binh:"))
-        k = float(input("Gioi han khach hang trong hang doi:"))
-    except BaseException:
-        clear()
-        print("Invalid")
-        display_M_M_s_k()
-        
-    mar = M_M_S_K(s,x,y,k)
-    print("Số khách hàng trung bình trong hệ thống: " + str(mar.L()))
-    print("Số khách hàng trung bình trong hàng chờ: " + str(mar.Lq()))
-    print("Thời gian phục trung bình: "+ str(mar.W()))
-    print("Thời gian chờ trung bình: "+ str(mar.Wq()))
-    print("Hiệu suất phục vụ của hệ thống: "+ str(mar.P()))
-    input()
-    print("1. Tiếp tục")
-    print("2. Trở lại")
-    i = input()
-    if i == '1':
-       clear()
-       display_M_M_s_k()
     elif i == '2':
         clear()
         displayMenu()
